03c1f57ba84da2d177027bd6f699709612105374
Karan Singh <singh.karangkp@gmail.com>
2026-09-25 16:27:40 +0530

    feat: implemented encryption and decryption using aes-256

--- crypto.go
+++ crypto.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 
 	"golang.org/x/crypto/argon2"
@@ -16,5 +18,42 @@ func GenerateSalt() ([]byte, error) {
 }
 
 func DeriveKey(password []byte, salt []byte) []byte {
-	return argon2.IDKey(password, salt, 1, 64*1024, 4, 32)
+	return argon2.IDKey(password, salt, 1, 64*1024, 4, 32) // takes 64MB of ram
+}
+
+func Encrypt(key []byte, plaintext []byte) (ciphertext []byte, nonce []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	nonce = make([]byte, aead.NonceSize())
+	_, err = rand.Read(nonce)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ciphertext = aead.Seal(nil, nonce, plaintext, nil)
+	return ciphertext, nonce, nil
+}
+
+func Decrypt(key []byte, nonce []byte, ciphertext []byte) (plaintext []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	aead, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	plaintext, err = aead.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plaintext, nil
 }
